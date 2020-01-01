33debb2e30980e5f5143f957b3058bdbe63b8cb8
Ramazan Celep <rcelep@gmail.com>
2020-01-01 01:54:03 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ function logWhisper(string) {
 function sayHiToGrandma(string) {
   if (string === "hello") {
     return 'I can\'t hear you!'
-  } else if () {
+  } else {
     return 'YES INDEED!'
   }
 }
